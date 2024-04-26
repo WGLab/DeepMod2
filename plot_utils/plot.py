fixe dmotif bug, added standard normalization

plot_utils/plot.py
@@ -3,7 +3,7 @@ import numpy as np
 from pathlib import Path
 import os, pysam
 import pandas as pd
-
+from scipy import stats
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
@@ -261,7 +261,7 @@ def violin_plot(sample_data, avg_type='median', static_display=False, meanline_v
     return 
     
     
-def compare_violin_plot(sample1_data, sample2_data, label1='Mod', label2='Unmod', avg_type='median', static_display=False, meanline_visible=False, figure_width=1000, figure_height=500, save_path=None):
+def compare_violin_plot(sample1_data, sample2_data, label1='Mod', label2='Unmod', avg_type='median', static_display=False, meanline_visible=False, figure_width=1000, figure_height=500, save_path=None, test_type='mw', test_method="auto", display_pval=True):
     tmp_dict=dict(sample1_data)
     tmp_dict.update(sample2_data)
     cons_seq=get_consensus(tmp_dict)
@@ -317,9 +317,25 @@ def compare_violin_plot(sample1_data, sample2_data, label1='Mod', label2='Unmod'
                             meanline_visible=meanline_visible)
                  )
     
+    
+    group_df=df.groupby(['Position', 'Sample'])['Signal'].apply(list)
+    dist_stats={}
+    for i in range(len(group_df)//2):
+        if test_type=='ks':
+            s=stats.ks_2samp(group_df.loc[i,:].loc[label1], group_df.loc[i,:].loc[label2], method=test_method)
+        elif test_type=="mw":
+            s=stats.mannwhitneyu(group_df.loc[i,:].loc[label1], group_df.loc[i,:].loc[label2], method=test_method)
+        dist_stats[i]={'Position':i,'Base':cons_seq[i],'Statistic':s.statistic, 'Pvalue':s.pvalue}
+
+    dist_stats_df=pd.DataFrame(dist_stats).T
+    dist_stats_df=dist_stats_df.astype({'Position': 'int', 'Statistic': 'float32', 'Pvalue': 'float32',})
+
     tickvals=np.arange(0,len(cons_seq))
-    ticktext=['%d<br>%s' %(a-len(cons_seq)//2,b) for a,b in zip(tickvals, cons_seq)]
-    fig.update_xaxes(tickmode='array', tickvals=tickvals, ticktext=ticktext)
+    if display_pval:
+        ticktext=['{}<br>{}<br>{:0.1e}'.format(a-len(cons_seq)//2,b,c) for a,b,c in zip(tickvals, cons_seq, dist_stats_df.Pvalue)]
+    else:
+        ticktext=['%d<br>%s' %(a-len(cons_seq)//2,b) for a,b in zip(tickvals, cons_seq)]
+    fig.update_xaxes(tickmode='array', tickvals=tickvals, ticktext=ticktext,tickfont = dict(size = 8))
     fig.update_layout(violingap=0, violinmode='overlay')
     
     if static_display:
@@ -332,7 +348,7 @@ def compare_violin_plot(sample1_data, sample2_data, label1='Mod', label2='Unmod'
             fig.write_html(save_path)
         fig.show()
     
-    return 
+    return dist_stats_df
 
 
 def revcomp(s):
@@ -419,10 +435,13 @@ def get_read_signal_raw(signal, move,norm_type):
 
     return norm_signal, base_level_data
 
-def get_signals(bam_path, chrom, pos, strand, read_filename_dict, base_path, seq_type, max_cov=1000, window=10, norm_type='STD'):
+def get_signals(bam_path, chrom, pos, strand, read_filename_dict, base_path, seq_type, max_cov=1000, window_before=10, window_after=10, norm_type='STD'):
     read_info=get_read_positions(bam_path, chrom, pos, strand,seq_type)
     data={}
     
+    if seq_type=='rna':
+        window_before, window_after=window_after, window_before
+        
     cov=0
     for read_name in read_info.keys():
         if cov > max_cov:
@@ -455,13 +474,13 @@ def get_signals(bam_path, chrom, pos, strand, read_filename_dict, base_path, seq
                 
                 if seq_type=='rna':
                     fq=fq[::-1]
-                
+                    
                 norm_signal, base_level_data = get_read_signal_raw(signal, move, norm_type)
                 seq_len=len(fq)
                 
-                if read_pos>window+5 and read_pos<seq_len-window-1-5:
+                if read_pos>window_before+5 and read_pos<seq_len-window_after-1-5:
                     cov+=1
-                    base_seq=fq[read_pos-window : read_pos+window+1]
+                    base_seq=fq[read_pos-window_before : read_pos+window_after+1]
                     
                     data[read_name]={}
                     
@@ -469,14 +488,13 @@ def get_signals(bam_path, chrom, pos, strand, read_filename_dict, base_path, seq
                     
                     if seq_type=='dna':
                         data[read_name]['seq']=base_seq
-                        for x in range(read_pos-window, read_pos+window+1):
+                        for x in range(read_pos-window_before, read_pos+window_after+1):
                             norm.append(np.array(norm_signal[base_level_data[x][0]:base_level_data[x][0]+base_level_data[x][1]]))
                         data[read_name]['signal']=norm
                         
                     elif seq_type=='rna':
                         data[read_name]['seq']=base_seq[::-1]
-                        
-                        for x in range(read_pos-window, read_pos+window+1):
+                        for x in range(read_pos-window_before, read_pos+window_after+1):
                             norm.append(np.array(norm_signal[base_level_data[x][0]:base_level_data[x][0]+base_level_data[x][1]][::-1]))
                         data[read_name]['signal']=norm[::-1]
 

src/detect.py
@@ -446,7 +446,7 @@ def process(params,ref_pos_dict, signal_Q, output_Q, input_event, ref_seq_dict):
     total_ref_seq_list=[]
     r_count=0
     
-    dummy_ref_seq=4+np.zeros(21)
+    dummy_ref_seq=4+np.zeros(2*window+1)
     
     ref_available=True if params['ref'] else False
     
@@ -551,7 +551,7 @@ def process(params,ref_pos_dict, signal_Q, output_Q, input_event, ref_seq_dict):
                 base_seq_list=np.vstack(total_base_seq_list)
                 ref_seq_list=np.vstack(total_ref_seq_list)
                 
-                pred_list=[model(batch_x.to(dev), batch_base_seq.to(dev), batch_ref_seq.to(dev)).cpu().numpy() for batch_x, batch_base_seq, batch_ref_seq in generate_batches(features_list, base_seq_list, ref_seq=ref_seq_list, batch_size = chunk_size)]
+                pred_list=[model(batch_x.to(dev), batch_base_seq.to(dev), batch_ref_seq.to(dev)).cpu().numpy() for batch_x, batch_base_seq, batch_ref_seq in generate_batches(features_list, base_seq_list, window, ref_seq=ref_seq_list, batch_size = chunk_size)]
 
                                 
                 pred_list=np.vstack(pred_list)
@@ -575,7 +575,7 @@ def process(params,ref_pos_dict, signal_Q, output_Q, input_event, ref_seq_dict):
         base_seq_list=np.vstack(total_base_seq_list)
         ref_seq_list=np.vstack(total_ref_seq_list)
         
-        pred_list=[model(batch_x.to(dev), batch_base_seq.to(dev), batch_ref_seq.to(dev)).cpu().numpy() for batch_x, batch_base_seq, batch_ref_seq in generate_batches(features_list, base_seq_list, ref_seq=ref_seq_list, batch_size = chunk_size)]
+        pred_list=[model(batch_x.to(dev), batch_base_seq.to(dev), batch_ref_seq.to(dev)).cpu().numpy() for batch_x, batch_base_seq, batch_ref_seq in generate_batches(features_list, base_seq_list, window, ref_seq=ref_seq_list, batch_size = chunk_size)]
 
         pred_list=np.vstack(pred_list)
         pred_list=np.split(pred_list.ravel(), read_counts)

src/utils.py
@@ -128,9 +128,9 @@ def get_model(params):
         print('Model: %s not found.' %params['model'], flush=True)
         sys.exit(2)
 
-def generate_batches(features, base_seq, ref_seq=None, batch_size=512):        
+def generate_batches(features, base_seq, window, ref_seq=None, batch_size=512):        
         if len(ref_seq)==0:
-            ref_seq=(4+torch.zeros(features.shape[0], 21)).type(torch.LongTensor)       
+            ref_seq=(4+torch.zeros(features.shape[0], 2*window+1)).type(torch.LongTensor)       
         else:
             ref_seq=torch.Tensor(ref_seq).type(torch.LongTensor)
             

train/generate_features.py
@@ -10,7 +10,7 @@ from ont_fast5_api.fast5_interface import get_fast5_file
 from numba import jit
 import queue, gzip
 import pod5 as p5
-
+import utils
 
 base_to_num_map={'A':0, 'C':1, 'G':2, 'T':3, 'U':3,'N':4}
 
@@ -54,7 +54,7 @@ def motif_check(motif):
         motif_seq=motif[0]
         motif_ind=[int(x) for x in motif[1:]]
         
-        if len(set(motif_seq[x] for x in motif_ind))!=1 or len(set(motif_seq[0])-set('ACGT'))>0:
+        if len(set(motif_seq[x] for x in motif_ind))!=1 or len(set(motif_seq[x] for x in motif_ind)-set('ACGT'))>0:
             print('Base of interest should be same for all indices and must be one of A, C, G or T.', flush=True)
             return motif_seq, exp_motif_seq, final_motif_ind, valid
         
@@ -161,8 +161,8 @@ def get_ref_info(args):
     
     fwd_pos_array, rev_pos_array=None, None
     if motif_seq:
-        fwd_motif_anchor=np.array([m.start(0) for m in re.finditer(r'{}'.format(exp_motif_seq), seq)])
-        rev_motif_anchor=np.array([m.start(0) for m in re.finditer(r'{}'.format(revcomp(exp_motif_seq)), seq)])
+        fwd_motif_anchor=np.array([m.start(0) for m in re.finditer(r'(?={})'.format(exp_motif_seq), seq)])
+        rev_motif_anchor=np.array([m.start(0) for m in re.finditer(r'(?={})'.format(revcomp(exp_motif_seq)), seq)])
 
         fwd_pos_array=np.array(sorted(list(set.union(*[set(fwd_motif_anchor+i) for i in motif_ind])))).astype(int)
         rev_pos_array=np.array(sorted(list(set.union(*[set(rev_motif_anchor+len(motif_seq)-1-i) for i in motif_ind])))).astype(int)
@@ -170,12 +170,18 @@ def get_ref_info(args):
     return chrom, seq_array, fwd_pos_array, rev_pos_array
 
 @jit(nopython=True)
-def get_events(signal, move):
+def get_events(signal, move, norm_type):
     stride, start, move_table=move
-    median=np.median(signal)
-    mad=np.median(np.abs(signal-median))
     
-    signal=(signal-median)/mad
+    if norm_type=='mad':
+        median=np.median(signal)
+        mad=np.median(np.abs(signal-median))
+        signal=(signal-median)/mad
+        
+    else:
+        mean=np.mean(signal)
+        std=np.std(signal)
+        signal=(signal-mean)/std
     
     signal[signal>5]=5
     signal[signal<-5]=-5
@@ -220,8 +226,8 @@ def get_pos(path):
     
     return labelled_pos_list
 
-def write_to_npz(output_file_path, mat, base_qual, base_seq, ref_seq, label, ref_coordinates, read_name, ref_name):
-    np.savez(output_file_path, mat=mat, base_qual=base_qual, base_seq=base_seq, ref_seq=ref_seq, label=label, ref_coordinates=ref_coordinates, read_name=read_name, ref_name=ref_name)
+def write_to_npz(output_file_path, mat, base_qual, base_seq, ref_seq, label, ref_coordinates, read_name, ref_name, window, norm_type):
+    np.savez(output_file_path, mat=mat, base_qual=base_qual, base_seq=base_seq, ref_seq=ref_seq, label=label, ref_coordinates=ref_coordinates, read_name=read_name, ref_name=ref_name, window=window, norm_type=norm_type)
                        
 def get_output(params, output_Q, process_event):
     output=params['output']
@@ -278,7 +284,7 @@ def get_output(params, output_Q, process_event):
                         print('%s: Number of reads processed = %d.' %(str(datetime.datetime.now()), read_count), flush=True)
                         
                         
-                        write_to_npz(output_file_path, mat, base_qual, base_seq, ref_seq, label, ref_coordinates, read_name, ref_name)
+                        write_to_npz(output_file_path, mat, base_qual, base_seq, ref_seq, label, ref_coordinates, read_name, ref_name, window=params['window'], norm_type=params['norm_type'])
 
                         chunk+=1
                         output_file_path=os.path.join(output,'%s.features.%d.npz' %(params['prefix'], chunk))
@@ -311,7 +317,7 @@ def get_output(params, output_Q, process_event):
         print('%s: Number of reads processed = %d.' %(str(datetime.datetime.now()), read_count), flush=True)
 
 
-        write_to_npz(output_file_path, mat, base_qual, base_seq, ref_seq, label, ref_coordinates, read_name, ref_name)
+        write_to_npz(output_file_path, mat, base_qual, base_seq, ref_seq, label, ref_coordinates, read_name, ref_name, params['window'], params['norm_type'])
 
     return
 
@@ -320,7 +326,8 @@ def process(params, ref_pos_dict, signal_Q, output_Q, input_event, ref_seq_dict,
     
     window=params['window']
     window_range=np.arange(-window,window+1)
-    
+    norm_type=params['norm_type']
+                     
     div_threshold=params['div_threshold']
     cigar_map={'M':0, '=':0, 'X':0, 'D':1, 'I':2, 'S':2,'H':2, 'N':1, 'P':4, 'B':4}
     cigar_pattern = r'\d+[A-Za-z]'
@@ -392,7 +399,7 @@ def process(params, ref_pos_dict, signal_Q, output_Q, input_event, ref_seq_dict,
             mean_qscore=-10*np.log10(np.mean(base_qual))
             base_qual=(1-base_qual)
             
-            mat=get_events(signal, move)
+            mat=get_events(signal, move, norm_type)
             
             if seq_type=='rna':
                 mat=np.flip(mat,axis=0)
@@ -606,6 +613,8 @@ if __name__ == '__main__':
     parser.add_argument("--motif", help='Motif for generating features followed by zero-based indices of nucleotides within the motif to generate features for. The motif and each index listed should be separated by whitespace, e.g. "--motif CGCG 0 2". Use either --pos_list or --motif to specify how to choose loci for feature generation, but not both. Features will be generated for all loci of the read that map to a reference sequence that matches the motif. Multiple indices can be specified but they should refer to the same nucleotide letter.  If you use --motif, it is assumed that all loci have the same modification label and you need to specify the label using --motif_label.', nargs='*')
     
     parser.add_argument("--motif_label", help='Modification label for the motif. 0 is for unmodified and 1 is for modified.',type=int, choices=[0,1])
+    
+    parser.add_argument("--norm_type", help='How to normalize read signal.',type=str, choices=['mad', 'standard'], default='mad')
         
     args = parser.parse_args()
     
@@ -621,7 +630,6 @@ if __name__ == '__main__':
         chrom_list=pysam.Samfile(args.bam).references
 
         
-     
     if args.motif and len(args.motif)>0:
         if args.pos_list is not None:
             print('Use either --motif or --pos_list but not both', flush=True)
@@ -631,12 +639,12 @@ if __name__ == '__main__':
             print('--motif_label should be specified with --motif option', flush=True)
             sys.exit(3)
         
-        motif_seq, exp_motif_seq, motif_ind, valid_motif=utils.motif_check(args.motif)
+        motif_seq, exp_motif_seq, motif_ind, valid_motif=motif_check(args.motif)
         if not valid_motif:
             sys.exit(3) 
             
     else:
-        motif=None
+        motif_seq=None
         motif_ind=None
         exp_motif_seq=None
         
@@ -650,7 +658,8 @@ if __name__ == '__main__':
             pos_list=args.pos_list, 
             ref=args.ref, 
             input=args.input,
-            motif_seq=motif,
+            norm_type=args.norm_type,
+            motif_seq=motif_seq,
             motif_ind=motif_ind,
             exp_motif_seq=exp_motif_seq,
             motif_label=args.motif_label,

train/train_models.py
@@ -1,23 +1,16 @@
-import math, time, argparse, re
-from torch.utils.data import Dataset,DataLoader
+import math, time, argparse, re,  os, sys
 import functools, itertools, random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch import optim, Tensor
 import numpy as np
 import multiprocessing as mp
-import os, sys
-from torch import optim
-from torch import nn, Tensor
-import pathlib
-from sklearn.model_selection import train_test_split
 import multiprocessing as mp
 import queue
 import pickle
-from pathlib import Path
-from sklearn.model_selection import train_test_split
-import sklearn
+from utils import *
 
 class OneHotEncode(nn.Module):
     def __init__(self, num_classes: int):
@@ -219,150 +212,9 @@ class TransformerModel(nn.Module):
         
         return x
 
-def get_files(input_list):
-    out_list=[]
-    if type(input_list)==type(None):
-        return out_list
-    for item in input_list:
-        
-        if os.path.isdir(item):
-            out_list.extend(list(Path(item).rglob("*.npz")))
 
-        elif item[-4:]=='.npz':
-            out_list.append(item)        
-    
-    random.seed(0)
-    random.shuffle(out_list)
-    return out_list
-
-def read_from_file(file_name, validation_type, validation_fraction, data_type):
-    data=np.load(file_name)
-    mat=data['mat']
-    base_qual=data['base_qual']
-    
-    features=np.dstack((mat, base_qual[:,:,np.newaxis]))
-    features=torch.Tensor(features)
-    
-    base_seq=torch.Tensor(data['base_seq']).type(torch.LongTensor)
-    ref_seq=torch.Tensor(data['ref_seq']).type(torch.LongTensor)
-
-    labels=torch.Tensor(data['label'][:,np.newaxis])
-    
-    if validation_type=='split':
-        features_train, features_test, base_seq_train, base_seq_test, \
-        ref_seq_train, ref_seq_test, labels_train, labels_test=\
-        train_test_split(features, base_seq, ref_seq, labels, test_size=validation_fraction, random_state=42)
-        if data_type=='train':
-            return features_train, base_seq_train, ref_seq_train, labels_train
-        else:
-            return features_test, base_seq_test, ref_seq_test, labels_test
-        
-    else:
-        return features, base_seq, ref_seq, labels
-
-def generate_batches(files, validation_type, validation_fraction, data_type, batch_size=512):
-    counter = 0
-    
-    print_freq=max(1, len(files)//10)
-    
-    while counter<len(files):
-        file_name = files[counter]
-
-        counter +=1
-        
-        data=read_from_file(file_name, validation_type, validation_fraction, data_type)
-
-        features, base_seq, ref_seq, labels=data
-        batch_size=max(batch_size,1)
-        for local_index in range(0, labels.shape[0], batch_size):
-            batch_x=features[local_index:(local_index + batch_size)]
-            batch_base_seq=base_seq[local_index:(local_index + batch_size)]
-            batch_ref_seq=ref_seq[local_index:(local_index + batch_size)]
-            batch_y=labels[local_index:(local_index + batch_size)]          
-
-            yield batch_x, batch_base_seq, batch_ref_seq, batch_y
-        
-        if counter%print_freq==0:
-            print('.', end='',flush=True)
-
-def generate_batches_mixed_can_mod(data_file_list, validation_type, validation_fraction, data_type, batch_size=128):
-    
-    data_file_list=[x for x in data_file_list if len(x)!=0]
-
-    sizes=np.array([sum(len(np.load(f)['label']) for f in data) for data in data_file_list])
-    batch_sizes=(batch_size*sizes/np.sum(sizes)).astype(int)
-    batch_sizes[0]=batch_sizes[0]+batch_size-np.sum(batch_sizes)
-    
-    generators=[generate_batches(group_files, validation_type, validation_fraction, data_type, batch_size=group_batch_size) 
-                for group_files, group_batch_size in zip(data_file_list, batch_sizes)]
-    
-    
-    for multi_batch_data in zip(*generators):
-        batch_x=torch.vstack([d[0] for d in multi_batch_data if len(d[0])>0])
-        batch_base_seq=torch.vstack([d[1] for d in multi_batch_data if len(d[1])>0])
-        batch_ref_seq=torch.vstack([d[2] for d in multi_batch_data if len(d[2])>0])
-        batch_y=torch.vstack([d[3] for d in multi_batch_data if len(d[3])>0])
-        
-        yield batch_x, batch_base_seq, batch_ref_seq, batch_y
-
-def get_stats(metrics_dict, dtype):
-    
-    loss_str='{}  Loss:'.format(dtype)
-    acc_str='{} Accuracy:'.format(dtype)
-    prec_str='{} Precision:'.format(dtype)
-    rec_str='{} Recall:'.format(dtype)
-    f1_str='{} F1:'.format(dtype)
-    
-    for g in sorted(metrics_dict.keys()):
-        
-        acc=metrics_dict[g]['true']/max(1,metrics_dict[g]['len'])
-        loss=metrics_dict[g]['loss']/max(1,metrics_dict[g]['len'])
-        precision=metrics_dict[g]['TP']/max(1,metrics_dict[g]['TP']+metrics_dict[g]['FP'])
-        recall=metrics_dict[g]['TP']/max(1,metrics_dict[g]['TP']+metrics_dict[g]['FN'])
-        f1=2*precision*recall/(precision+recall) if precision*recall!=0 else 0
-
-        if len(metrics_dict.keys())==1:
-            x='Total'
-            total_acc=acc
-        else:
-            x=g
-            
-        loss_str+='  %s: %.4f' %(x, loss)
-        acc_str+='  %s: %.4f' %(x, acc)
-        prec_str+='  %s: %.4f' %(x, precision)
-        rec_str+='  %s: %.4f' %(x, recall)
-        f1_str+='  %s: %.4f' %(x, f1)
-        
-    if len(metrics_dict.keys())>1:
-        x='Total'
-        
-        acc=sum(f['true'] for f in metrics_dict.values())/max(1,sum(f['len'] for f in metrics_dict.values()))
-        loss=sum(f['loss'] for f in metrics_dict.values())/max(1,sum(f['len'] for f in metrics_dict.values()))
-        precision=sum(f['TP'] for f in metrics_dict.values())/max(1,sum(f['TP'] for f in metrics_dict.values()) +sum(f['FP'] for f in metrics_dict.values()))
-        recall=sum(f['TP'] for f in metrics_dict.values())/max(1,sum(f['TP'] for f in metrics_dict.values())+sum(f['FN'] for f in metrics_dict.values()))
-        f1=2*precision*recall/(precision+recall) if precision*recall!=0 else 0
-        
-        total_acc=acc
-        
-        loss_str+='  %s: %.4f' %(x, loss)
-        acc_str+='  %s: %.4f' %(x, acc)
-        prec_str+='  %s: %.4f' %(x, precision)
-        rec_str+='  %s: %.4f' %(x, recall)
-        f1_str+='  %s: %.4f' %(x, f1)
-    
-    return '\n'.join([loss_str, acc_str,prec_str, rec_str, f1_str]), total_acc
-
-def get_metrics(metrics_dict,name, batch_y, score, loss):
-    eval_counts=sklearn.metrics.confusion_matrix(batch_y.cpu(),(score>0).cpu(),labels=[0,1])
-    metrics_dict[name]['len']+=len(batch_y)
-    metrics_dict[name]['TP']+=eval_counts[1,1]
-    metrics_dict[name]['FP']+=eval_counts[0,1]
-    metrics_dict[name]['FN']+=eval_counts[1,0]
-    metrics_dict[name]['true']=metrics_dict[name]['true']+eval_counts[0,0]+eval_counts[1,1]
-    metrics_dict[name]['loss']+=loss.item()*len(batch_y)
     
 def train(training_dataset, validation_dataset, validation_type, validation_fraction, model_config, epochs, prefix, retrain, batch_size, args_str, seed):
-    
     print('Starting training.' , flush=True)
     torch.manual_seed(seed)
     model_type = model_config['model_type']
@@ -543,7 +395,6 @@ if __name__=='__main__':
 
     parser.add_argument("--fc_type", help='Type of full connection to use in the classifier.', type=str, default='all', choices=['middle', 'all'])
     parser.add_argument("--model_type", help='Type of model to use', type=str, choices=['bilstm', 'transformer'])
-    parser.add_argument("--window", help='Number of bases before and after the modified base of interest used in feature generation', type=int)
 
     parser.add_argument("--num_layers", help='Number of transformer encoder or BiLSTM layers', type=int, default=3)
     parser.add_argument("--dim_feedforward", help='Dimension of feedforward layers in  transformer encoder or size of hidden units in BiLSTM layers', type=int, default=100)
@@ -571,14 +422,20 @@ if __name__=='__main__':
     validation_dataset=get_files(args.validation_dataset)
     validation_type=args.validation_type
     validation_fraction=args.validation_fraction
+
+    valid_data, window, norm_type = check_training_files(mixed_training_dataset, can_training_dataset,\
+                                           mod_training_dataset, validation_dataset)
     
-    model_config = dict(model_dims=(2*args.window+1,10),window=args.window, model_type=args.model_type,
+    if not valid_data:
+        sys.exit(3)
+
+    model_config = dict(model_dims=(2*window+1,10),window=window, model_type=args.model_type,
     num_layers=args.num_layers, dim_feedforward=args.dim_feedforward,
     num_fc=args.num_fc, embedding_dim=args.embedding_dim,
     embedding_type=args.embedding_type, include_ref=args.include_ref,
     pe_dim=args.pe_dim, nhead=args.nhead, pe_type=args.pe_type,
     l2_coef=args.l2_coef, lr=args.lr, model_save_path=args.model_save_path, fc_type=args.fc_type,
-    train_w_wo_ref=args.train_w_wo_ref, weights=args.weights)
+    train_w_wo_ref=args.train_w_wo_ref, weights=args.weights, norm_type=norm_type)
 
     args_dict=vars(args)
     args_str=''.join('%s: %s\n' %(k,str(v)) for k,v in args_dict.items())